e112cff56eee7b6f95908324960c5c9cdbf5bc66
riki <riki@namu.finance>
2022-08-31 16:11:25 +0900

    first commit

---
+++ src/error.rs
@@ -0,0 +1,17 @@
+use thiserror::Error;
+
+use solana_program::program_error::ProgramError;
+
+use crate::{instruction::EscrowInstruction, error::EscrowError};
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
